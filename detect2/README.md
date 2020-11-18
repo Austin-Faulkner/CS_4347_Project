5e86d094192ece674803233bb5dcafb784378491
Austin Faulkner <faulkner.austin@gmail.com>
2020-11-18 13:42:12 -0600

    first commit - Austin Faulkner

---
+++ detect2/README.md
@@ -0,0 +1,3 @@
+In this folder are all the essential Detectron2 files for Mask R-CNN, Instance
+Segmentation, Keypoint Detection, Panoptic Segmentation and the LVIS and COCO
+datasets.
---
+++ mask r-cnn (more code - Austin Faulkner)/near_ut_austin_panopt_instance_guad.py
@@ -0,0 +1,54 @@
+from google.colab import drive
+
+drive.mount('/content/drive', force_remount=True)
+
+# install dependencies:
+!pip install pyyaml==5.1 'pycocotools>=2.0.1'
+!pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+
+import torch, torchvision
+print(torch.__version__, torch.cuda.is_available())
+!gcc --version
+# opencv is pre-installed on colab
+
+# install detectron2: (Colab has CUDA 10.1 + torch 1.6)
+assert torch.__version__.startswith("1.6")
+!pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.6/index.html
+
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+from google.colab.patches import cv2_imshow
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+# This is the video we're going to process
+from IPython.display import YouTubeVideo, display
+video = YouTubeVideo("BvuOCEyky8o", width=500)
+display(video)
+
+!pip install youtube-dl
+!pip uninstall -y opencv-python-headless opencv-contrib-python
+!apt install python3-opencv  # the one pre-installed have some issues
+!youtube-dl https://youtu.be/BvuOCEyky8o -f 22 -o video.mp4
+!ffmpeg -i video.mp4 -t 00:01:12 -c:v copy video-clip.mp4
+
+!git clone https://github.com/facebookresearch/detectron2
+
+!python detectron2/demo/demo.py --config-file detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml --video-input video-clip.mp4 --confidence-threshold 0.6 --output video-output.mkv \
+  --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x/138205316/model_final_a3ec72.pkl
+
+# Download the results
+from google.colab import files
+files.download('video-output.mkv')
